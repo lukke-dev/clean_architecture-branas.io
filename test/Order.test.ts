095c5efd54c79c14a4ccef732286eb24b4b145fd
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-07-23 18:51:10 -0300

    Aula 01

---
+++ test/Order.test.ts
@@ -0,0 +1,37 @@
+import Coupon from "../src/Coupon"
+import Item from "../src/Item"
+import Order from "../src/Order"
+
+test('Deve criar um pedido com CPF válido', function() {
+	const cpf = "839.435.452-10"
+	const order = new Order(cpf)
+	const total = order.getTotal()
+	expect(total).toBe(0)
+})
+
+test('Deve tentar criar um pedido com CPF inválido', function() {
+	const cpf = "111.111.111-11"
+	expect(() => new Order(cpf)).toThrow(new Error('Invalid cpf'))
+})
+
+test('Deve criar um pedido com 3 itens', function() {
+	const cpf = "839.435.452-10"
+	const order = new Order(cpf)
+	order.addItem(new Item(1, "Musica", "CD", 30), 3)
+	order.addItem(new Item(2, "Video", "DVD", 50), 1)
+	order.addItem(new Item(2, "Video", "VHS", 10), 2)
+	const total = order.getTotal()
+	expect(total).toBe(160)
+})
+
+test('Deve criar um pedido com 3 itens com cupom de desconto', function() {
+	const cpf = "839.435.452-10"
+	const order = new Order(cpf)
+	order.addItem(new Item(1, "Musica", "CD", 30), 3)
+	order.addItem(new Item(2, "Video", "DVD", 50), 1)
+	order.addItem(new Item(2, "Video", "VHS", 10), 2)
+	order.addCoupon(new Coupon("VALE20", 20))
+	const total = order.getTotal()
+	expect(total).toBe(128)
+})
+
